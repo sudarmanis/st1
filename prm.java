c1eea01d142c3eb32e3ab8b3f87edb9d28998ed4
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-13 07:27:27 -0700

    Create prm.java

---
+++ prm.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class prm
+{
+public static void main(String argS[])
+{
+int a, n,sr=0;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the no");
+a=sc.nextInt();
+System.out.println("intial value");
+n=sc.nextInt();
+for(int i=n;i<=a;i++)
+{
+if(a%i==0)
+{
+r++;
+}
+}
+if(r==2)
+{
+System.out.println("prime");
+}
+else
+{
+System.out.println(" not prime");
+}
+}}
