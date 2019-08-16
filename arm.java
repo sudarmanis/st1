1a0a6d60907973f6950d915fcb4dff0269509e2c
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-16 08:18:30 -0700

    Create arm.java

---
+++ arm.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class arm
+{
+public static void main(String argS[])
+{
+int n ,temp,re,s=0;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the initial no");
+n=sc.nextInt();
+{
+temp=n;
+ while(temp!=0)
+ {
+ re=temp%10;
+ s=s+re*re*re;
+ temp=temp/10;
+ }
+ if(s==n)
+ {
+ System.out.println("yes");
+ }
+ else
+ {
+	 
+ System.out.println("no");
+ }
+}}
+}
