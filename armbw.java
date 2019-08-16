3c72cd457494f2cbcd9ff20d7edb046205f23541
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-16 08:25:46 -0700

    Create armbw.java

---
+++ armbw.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class armbw
+{
+public static void main(String argS[])
+{
+int n,a ,temp,s=0,i,re;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the initial no");
+n=sc.nextInt();
+System.out.println("enter the end no");
+a=sc.nextInt();
+{
+for(i=n+1;i<a;i++)
+{
+temp=i;
+for(;temp!=0;)
+ {
+	re=temp%10;
+ s=s+re*re*re;
+ temp=temp/10;
+ }
+
+ if(s==i)
+ {
+ System.out.println(""+i);
+ }
+  s=0;
+
+}
+}
+}}
