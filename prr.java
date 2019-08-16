142802fa2822570bcd6cb46d8e76d41ccfa443d0
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-16 08:05:27 -0700

    Create prr.java

---
+++ prr.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class prr
+{
+public static void main(String argS[])
+{
+int n,a ,temp,s=0,i,j;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the initial no");
+n=sc.nextInt();
+System.out.println("enter the end no");
+a=sc.nextInt();
+{
+for(i=n+1;i<a;i++)
+{
+
+for(j=2;j<=a/10;j++)
+ {
+	 if(i%j==0)
+	 {
+ s=s+1;
+ }}
+ if(s==1)
+ {
+ System.out.println("palindrome" +""+i);
+ }
+  s=0;
+
+}
+
+}}}
