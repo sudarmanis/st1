414f642651e1b01476783fcd041b9e7f14e1a988
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-19 06:12:45 -0700

    Create mul.java

---
+++ mul.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class mul
+{
+public static void main(String argS[])
+{
+int n,i,a;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the mul no");
+n=sc.nextInt();
+for(i=1;i<=n;i++)
+{
+a=n*i;
+System.out.println(""+a);
+}
+}}
