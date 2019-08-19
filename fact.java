806795865bba989c0a3e72fab0b79eb90bed6a02
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-19 06:10:33 -0700

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class fact
+{
+public static void main(String argS[])
+{
+int n,i,a=1;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the mul no");
+n=sc.nextInt();
+for(i=1;i<=n;i++)
+{
+a=a*i;
+}
+System.out.println(""+a);
+}}
