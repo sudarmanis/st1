448475c54a54484a2c99a00fb192cdf3972a396c
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-24 08:05:44 -0700

    Update odd.java

--- odd.java
+++ odd.java
@@ -5,9 +5,7 @@ public static void main(String argS[])
 {
 int a, b,n;
 Scanner sc=new Scanner(System.in);
-System.out.println("enter the initial no");
 a=sc.nextInt();
-System.out.println("enter the end no");
 b=sc.nextInt();
 for(n=a+1;n<b;n++)
 {
